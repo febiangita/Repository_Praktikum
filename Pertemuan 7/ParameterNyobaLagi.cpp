2505bce8c6c01a47c45dd11bfd3f2db44373dabc
febiangita <febiangita925@gmail.com>
2025-12-18 23:30:44 +0700

    Create ParameterNyobaLagi.cpp

---
+++ Pertemuan 7/ParameterNyobaLagi.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+void ulangTahun(string nama, int umur){
+    cout << "Selamat Ulang Tahun " << nama << " yang ke-" << umur << "!" << endl;
+}
+
+int main(){
+    ulangTahun("Mamskyy", 58);
+    return 0;
+}
